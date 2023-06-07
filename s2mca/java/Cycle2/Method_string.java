d7a6e71fd8d0a8ece11d8fc1d2eb28984691068d
Angela133 <125428399+Angela133@users.noreply.github.com>
2023-06-07 16:04:09 +0530

    Add files via upload

---
+++ s2mca/java/Cycle2/Method_string.java
@@ -0,0 +1,9 @@
+class Method_string{
+public static void main(String[]args){
+String greet="Welcome to java";
+System.out.println("String: " +greet);
+
+int length=greet.length();
+System.out.println("length: " +length);
+}
+}
