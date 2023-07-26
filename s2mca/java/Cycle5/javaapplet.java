c2bd034b3f49be334cd95760b3c75acabad48a93
Angela133 <125428399+Angela133@users.noreply.github.com>
2023-07-26 16:01:33 +0530

    Add files via upload

---
+++ s2mca/java/Cycle5/javaapplet.java
@@ -0,0 +1,15 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class javaapplet extends Applet{
+    @Override
+    public void paint(Graphics g){
+        g.setColor(Color.BLACK);
+        g.drawLine(20,30,20,300);
+        g.drawRect(70,100,30,30);
+        g.fillRect(170,100,30,30);
+        g.drawOval(70,200,30,30);
+        g.fillOval(170,200,30,30);
+    }
+}
